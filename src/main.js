b5df3d0a1a1a0631ab0eda281c807f51f82a876d
Chris Combs <combsco@users.noreply.github.com>
2016-03-11 15:05:08 -0500

    Update main.js
    Removed some comments.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 
 import App from './App.vue'
-
 import Home from './Home.vue'
 import Login from './Login.vue'
 
@@ -34,7 +33,3 @@ router.beforeEach((transition) => {
 })
 
 router.start(App, 'body')
-
-// https://charlie-sample.auth0.com/api/v2/clients?fields=name%2Ctenant%2Cclient_id&include_fields=true
-// https://charlie-sample.auth0.com/api/v2/rules
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkcDlud2dTa3hWUVBhenVrR0wxTVVPcE9Ub1MzSms3eSIsInNjb3BlcyI6eyJydWxlcyI6eyJhY3Rpb25zIjpbInJlYWQiXX0sImNsaWVudHMiOnsiYWN0aW9ucyI6WyJyZWFkIl19fSwiaWF0IjoxNDU3NTI5MjM5LCJqdGkiOiI1ODkyMmJlMGFmNDI2MzVkM2MyYTM3MmIzMDI3MTFlOCJ9.tzfhQ3R2kadZmoTU7HPu8dQsIgRV1Ml5dxs0RA2JadY
